5afc5aea88f6f398154709a94e607fa9bb996572
Mónica <monicarossidcv@gmail.com>
2026-04-26 10:32:09 -0300

    inicio cambios sitio web

---
+++ js/funciones.js
@@ -0,0 +1,20 @@
+// Menú responsive //
+$(document).ready(function(){
+const menuToggle= document.querySelector(".menu-bars");
+const nav = document.querySelector("nav ul");
+
+menuToggle.addEventListener("click", () => {
+  nav.classList.toggle("menudesplegable");
+  });
+});
+
+// Efecto Scroll al hacer click en menú //
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 0) {
+    $('.navbar').addClass('scrolled');
+  }
+  if ($(window).scrollTop() < 100) {
+    $('.navbar').removeClass('scrolled');
+  }
+});
+
