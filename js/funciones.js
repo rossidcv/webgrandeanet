197e1fa84c8dadb0adb329ddcd392fcb7aca0dec
Mónica <monicarossidcv@gmail.com>
2026-05-05 18:38:10 -0300

    nuevos cambios sitio web

--- js/funciones.js
+++ js/funciones.js
@@ -19,3 +19,32 @@ $(window).scroll(function () {
 });
 
 
+
+$(document).ready(function(){
+  var flag = false;
+  var scroll;
+
+  $(window).scroll(function(){
+    scroll = $(window).scrollTop();
+
+    if(scroll > 0){
+      if(!flag){
+        $(".navbar").css({"background-color": "#6d5497"});
+        $(".navbar-nav li a").css({"color": "#fff"});
+        $(".navbar-nav li a.active").css({"color": "#ABC566"});
+        $(".navbar-brand h2").css({"color": "#fff"});
+        $(".navbar-brand p").css({"color": "#fff"});        
+        flag = true;
+      }
+    } 
+    else{
+      if(flag){
+        $(".navbar").css({"background-color": "rgba(248, 248, 248, 1)"});
+        $(".navbar-nav li a").css({"color": "#000"});
+        $(".navbar-brand h2").css({"color": "#000"});
+        $(".navbar-brand p").css({"color": "#000"});   
+        flag = false;
+      }
+    }       
+  });
+});
